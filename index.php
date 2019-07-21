f8168b361ab4f20ad3b2fdd0a5e8731be2da207e
Mazur <and.mazur5@gmail.com>
2019-07-21 19:54:12 +0200

    RWD_1

--- index.php
+++ index.php
@@ -13,7 +13,7 @@
     <body>
  
         
-<div class="test">
+<div class="main_oNas__oferta_background">
         <section id="main">
             <div class="main_photo">
                 <div class="col-8 main__content">
@@ -135,7 +135,7 @@
                         </ol>
                         <div class="carousel-inner">
                                     <div class="carousel-item active">
-                                        <img src="data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22800%22%20height%3D%22400%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20800%20400%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_16b79c71f67%20text%20%7B%20fill%3A%23555%3Bfont-weight%3Anormal%3Bfont-family%3AHelvetica%2C%20monospace%3Bfont-size%3A40pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_16b79c71f67%22%3E%3Crect%20width%3D%22800%22%20height%3D%22400%22%20fill%3D%22%23777%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%22285.9140625%22%20y%3D%22217.7%22%3EFirst%20slide%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E" alt="1" class="carousel__image">
+                                        <img src="data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22800%22%20height%3D%22400%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20800%20400%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_16b79c71f67%20text%20%7B%20fill%3A%23555%3Bfont-weight%3Anormal%3Bfont-family%3AHelvetica%2C%20monospace%3Bfont-size%3A40pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_16b79c71f67%22%3E%3Crect%20width%3D%22800%22%20height%3D%22400%22%20fill%3D%22%23777%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%22285.9140625%22%20y%3D%22217.7%22%3EFirst%20slide%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E" alt="1" class="carousel__imagecarousel__image">
                                         <div class="carousel-caption d-none d-md-block">
                                             <img src="images/galleries/referencje/1.jpg">
                                             <h5>Panna XYZ</h5>
@@ -280,17 +280,19 @@
 
     <div class="container">
         <div class="partners__title">
-            <h2 class="h2"> Partnerzy <h2>
+            <h2 class="h2"> Partnerzy </h2>
         <div>
         <div class="row">
-            <div class="position col-sm-12 col-md-4">
-                <img src="images/galleries/partnerzy/crossbox_morskieoko_logo.png" class=".img-fluid" >
-            </div>
-            <div class="position col-sm-12 col-md-4">
-                <img src="images/galleries/partnerzy/PZTP.jpg" class=".img-fluid">
-            </div>
-            <div class="position col-sm-12 col-md-4"> 
-                <img src="images/galleries/partnerzy/projekt_fabryczna_logo.png" class=".img-fluid">
+            <div class="partners-images">
+                <div class="position col-sm-4 col-md-4">
+                    <img src="images/galleries/partnerzy/crossbox_morskieoko_logo.png" class=".img-fluid" >
+                </div>
+                <div class="position col-sm-4 col-md-4">
+                    <img src="images/galleries/partnerzy/PZTP.jpg" class=".img-fluid">
+                </div>
+                <div class="position col-sm-4 col-md-4"> 
+                    <img src="images/galleries/partnerzy/projekt_fabryczna_logo.png" class=".img-fluid">
+                </div>
             </div>
         <div>
     </div>
